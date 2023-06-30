4384565b3c12178ebfb70c361550d0988023c7f9
vincent lipinski <lipinskivincent@gmail.com>
2023-06-30 11:51:16 +0200

    ECF3

--- src/main/java/com/example/ecf3/service/GameService.java
+++ src/main/java/com/example/ecf3/service/GameService.java
@@ -61,6 +61,12 @@ public class GameService {
         throw new NotSignInException();
     }
 
+    public Game getGameById(int id) throws NotSignInException {
+        if(_loginService.isLogged()) {
+            return _gameRepository.getGameById(id);
+        }
+        throw new NotSignInException();
+    }
 
 
 }
